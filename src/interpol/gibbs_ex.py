5103c524997e7c7dd6a0da8ef552bcdbf4200bb2
Alvaro Osses <aossesn@gmail.com>
2026-08-12 22:59:32 -0400

    avance interpool, acomienzo ajuste

--- src/interpol/gibbs_ex.py
+++ src/interpol/gibbs_ex.py
@@ -18,20 +18,17 @@ def lagrange(x, xi, yi):
 
 N = np.random.randint(101)
 
-xi = np.linspace(0, 10, 10) #10*np.random.rand(N)
-fi = np.exp(-xi**2) + np.sin(xi) #np.random.rand(100)
-
-#fi = xi + 0.1*np.random.randint(1, 10, xi.size)
+xi = np.linspace(0, 1, 10) #10*np.random.rand(N)
+fi = xi + 0.1*np.random.randint(1, 10, xi.size)
 
 x = np.linspace(np.min(xi), np.max(xi), 100)
 
 
 plt.scatter(xi, fi)
-plt.plot(x, np.sin(x) + np.exp(-x**2), label = "$f(x) = e^{-x^{2}} + \\sin(x)$")
 plt.plot(x, lagrange(x, xi, fi), "r--", label = "Polinomio interpolador")
+#plt.plot(x, np.sin(x) + np.exp(-x**2), label = "$f(x) = e^{-x^{2}} + \\sin(x)$")
 plt.legend()
 
-plt.savefig("../../img/interpol/lagrange.pdf")
+plt.savefig("../../img/interpol/gibbs.pdf")
 
-#plt.show()
-
+plt.show()
